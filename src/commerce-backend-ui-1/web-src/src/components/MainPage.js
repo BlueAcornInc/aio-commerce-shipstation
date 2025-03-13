fa01728dcb0aa10f0a4a2ef78bf78b2c28501687
Daniel Zaretsky <daniel.zaretsky@blueacornici.com>
2025-03-13 10:53:29 -0700

    Working state lib example

--- src/commerce-backend-ui-1/web-src/src/components/MainPage.js
+++ src/commerce-backend-ui-1/web-src/src/components/MainPage.js
@@ -2,8 +2,7 @@ import ShipstationConfigForm from "./ShipstationConfigForm";
 
 export const MainPage = props => {
     const namespace = process.env.AIO_runtime_namespace
-    const actionUrl = `https://${namespace}.adobeio-static.net/api/v1/web/ShipStation/commerce-rest-get`
-
+    const actionUrl = `https://35582-shipstationlocal-local.adobeioruntime.net/api/v1/web/aio-commerce-shipstation-app/shipstation-config`
     return (
         <ShipstationConfigForm actionUrl={actionUrl} />
     )
