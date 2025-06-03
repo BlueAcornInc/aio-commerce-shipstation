dccefe3795b2a608a16de6639fd9990b4968a9bb
Doug Hatcher <doug.hatcher@blueacornici.com>
2025-06-03 20:56:25 +0000

    fleshes out a few things

--- src/commerce-backend-ui-1/actions/registration/index.js
+++ src/commerce-backend-ui-1/actions/registration/index.js
@@ -1,38 +1,31 @@
 // src/commerce-backend-ui-1/actions/registration/index.js
-async function main(params) {
-  params;
-  const namespace = process.env.__OW_NAMESPACE || "default-namespace";
-  const baseUrl = `https://${namespace}.adobeio-static.net`;
-  const href = `${baseUrl}/index.html`;
-  return {
-    statusCode: 200,
-    body: {
-      registration: {
-        name: "shipstation-extension",
-        title: "ShipStation Extension",
-        description:
-          "ShipStation out-of-process shipping integration for Adobe Commerce",
-        icon: "none",
-        publisher: "PUBLISHER_ID",
-        status: "PUBLISHED",
+async function main() {
+    const extensionId = 'ShipStation'
 
-        endpoints: {
-          "commerce/backend-ui/1": {
-            view: [
-              {
-                href: href,
-              },
-            ],
-          },
-        },
-
-        xrInfo: {
-          supportEmail: "extensions@blueacorn.com",
-          appId: "APP_ID",
-        },
-      },
-    },
-  };
+    return {
+        statusCode: 200,
+        body: {
+            registration: {
+                menuItems: [
+                    {
+                        id: `${extensionId}::first`,
+                        title: 'ShipStation',
+                        parent: `${extensionId}::apps`,
+                        sortOrder: 1
+                    },
+                    {
+                        id: `${extensionId}::apps`,
+                        title: 'ShipStation',
+                        isSection: false,
+                        sortOrder: 100
+                    }
+                ],
+                page: {
+                    title: 'ShipStation'
+                }
+            }
+        }
+    }
 }
 
-exports.main = main;
+exports.main = main
